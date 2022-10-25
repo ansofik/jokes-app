09328ced75d3a1ed3dda1b3ceddd52ff7dc809f4
sofia <84174004+sofiak0@users.noreply.github.com>
2022-10-25 22:50:32 +0300

    get jokes from localStorage when offline

--- src/App.js
+++ src/App.js
@@ -5,83 +5,97 @@ import axios from 'axios';
 function reducer(state, action) {
   switch (action.type) {
 
-    case 'ADD_JOKE':
-      return {
-        ...state,
-        jokes: state.jokes.concat({ value: action.payload.value, id: action.payload.id })
-      }
-
     case 'GET_NEW_JOKE':
-      return { ...state, getNewJoke: true }
-
-    case 'GET_JOKES_AUTOMATICALLY':
-      return { ...state, getAuto: true }
-
-    case 'GET_JOKES_MANUALLY':
-      return { ...state, getAuto: false }
+      return { ...state, getNewJoke: true };
 
     case 'REQUEST_FINISHED':
-      return { ...state, getNewJoke: false }
+      return {
+        ...state,
+        joke: action.payload,
+        getNewJoke: false
+      };
+
+    case 'GET_JOKES_AUTOMATICALLY':
+      return { ...state, getAuto: true };
+
+    case 'GET_JOKES_MANUALLY':
+      return { ...state, getAuto: false };
 
     case 'SAVE_INTERVAL_ID':
-      return { ...state, intervalId: action.payload }
+      return { ...state, intervalId: action.payload };
 
     default:
-      throw new Error("Action.type unknown")
+      throw new Error("Action.type unknown");
   }
 }
 
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, { jokes: [], getNewJoke: false, getAuto: false, intervalId: null });
+  const [data, dispatch] = useReducer(reducer, { joke: {}, getNewJoke: false, getAuto: false, intervalId: null });
 
-  // new joke is fetched iff getNewJoke is true
+  function getRandomInt(upperBound) {
+    return Math.floor(Math.random() * upperBound);
+  }
+
+  // new joke is fetched iff getNewJoke changed to true
   useEffect(() => {
-    if (data.getNewJoke == true) {
+    if (data.getNewJoke === true) {
       async function getJoke() {
         try {
           let result = await axios('https://api.chucknorris.io/jokes/random');
-          dispatch({ type: 'REQUEST_FINISHED' })
-          dispatch({ type: 'ADD_JOKE', payload: { value: result.data.value, id: result.data.id } })
+          let newJoke = { value: result.data.value, id: result.data.id };
+          dispatch({ type: 'REQUEST_FINISHED', payload: newJoke });
+
+          // save joke to local storage
+          let jokesData = JSON.parse(localStorage.getItem('jokesData'));
+          if (jokesData == null) {
+            jokesData = [];
+          }
+          jokesData.push(newJoke);
+          localStorage.setItem('jokesData', JSON.stringify(jokesData));
+
         } catch {
-          console.log("Failed to retrieve a joke")
+          console.log("Failed to retrieve a joke from api");
+          // get joke from local storage instead
+          let jokesData = JSON.parse(localStorage.getItem('jokesData'));
+          dispatch({ type: 'REQUEST_FINISHED', payload: jokesData[getRandomInt(jokesData.length)] });
         }
       }
-      getJoke()
+      getJoke();
     }
-  }, [data.getNewJoke])
+  }, [data.getNewJoke]);
 
-  // jokes are fetched automatically every 10 seconds iff getAuto is true
+  /* jokes are fetched automatically every 10 seconds iff getAuto changed to true and
+  interval is removed iff getAuto changed to false */
   useEffect(() => {
     if (data.getAuto == false && data.intervalId !== null) {
-      console.log(data.intervalId)
-      clearInterval(data.intervalId)
+      clearInterval(data.intervalId);
     }
     if (data.getAuto == true) {
-      dispatch({ type: 'GET_NEW_JOKE' })
+      dispatch({ type: 'GET_NEW_JOKE' });
       dispatch({
         type: 'SAVE_INTERVAL_ID',
         payload: setInterval(() => dispatch({ type: 'GET_NEW_JOKE' }), 10000)
       })
     }
 
-  }, [data.getAuto])
+  }, [data.getAuto]);
 
 
   return (
     <div className="App">
-      <div className='buttons'>
-        {data.getAuto ? <button onClick={() => dispatch({ type: 'GET_JOKES_MANUALLY' })}>Hae vitsejä manuaalisesti</button>
-          : <div>
+      <div className="buttons">
+        {data.getAuto ? <button onClick={() => dispatch({ type: 'GET_JOKES_MANUALLY' })}>Hae vitsejä manuaalisesti</button> :
+          <div>
             <button onClick={() => dispatch({ type: 'GET_NEW_JOKE' })}>Hae vitsi</button>
             <button onClick={() => dispatch({ type: 'GET_JOKES_AUTOMATICALLY' })}>Hae vitsejä automaattisesti</button>
           </div>}
       </div>
+      {/* following div needs to have a changing key for animation to start over on render */}
       <div key={Math.random()} className='jokeContainer'>
-        {data.jokes.length > 0 && data.jokes[data.jokes.length - 1].value}
+        {Object.keys(data.joke).length > 0 && data.joke.value}
       </div>
     </div>
-
   );
 }
 
